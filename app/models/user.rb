36b1b7aa9deb56875172eade162e084247fc1f15
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2015-11-18 16:53:05 -0800

    moved methods from controller to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,4 +37,25 @@ class User < ActiveRecord::Base
 		user.authenticate(password_param)
 	end
 
+	# calculate net budget
+	def calculate_net_budget
+		fixed_expenses = budget.rent_mortgage + budget.car + budget.public_transportation + budget.insurance + budget.utilities + budget.internet + budget.cell_phone + budget.gym + budget.charity_donations + budget.student_loan + budget.credit_card + budget.other_expenses
+		return budget.net_wages - fixed_expenses
+	end
+
+	# calculate monthly savings based on net budget
+	def calculate_monthly_savings_goal
+		return (calculate_net_budget * 0.1).round
+	end
+
+	def calculate_daily_budget  
+	    # get net budget
+	    net_budget = calculate_net_budget	    
+	    # flexible spending less 10% savings
+	    flexible_spending = net_budget - calculate_monthly_savings_goal
+	    # return daily budget
+	    return flexible_spending / 30
+	end
+
+
 end
