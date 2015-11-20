e3072918105044f5dfd1743a7795d3b21b0f3a9e
ekingan <ekingan@gmail.com>
2015-11-20 10:04:06 -0800

    fixed it so that u is user

--- app/models/day.rb
+++ app/models/day.rb
@@ -4,15 +4,15 @@ class Day < ActiveRecord::Base
 	def self.create_new_days
 
 		#find all users and for each one:
-		User.all.each do |u|
+		User.all.each do |user|
 		#  create new key value pairs for that day for that user
 			@day = Day.new({
-				budget: u.calculate_daily_budget,
-				transactions_sum: u.yesterday_transaction_sum.to_i
+				budget: user.calculate_daily_budget,
+				transactions_sum: user.yesterday_transaction_sum.to_i
 				})
 
 			if @day.save
-				@user.days << @day
+				user.days << @day
 			end
 		end
 	end
