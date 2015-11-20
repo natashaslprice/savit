4fbe44627cf78d3ea93af8db159bbd86cd397b69
ekingan <ekingan@gmail.com>
2015-11-20 09:18:49 -0800

    added scheduler file and method

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,15 @@
 class Day < ActiveRecord::Base
 	belongs_to :user
+
+	def self.create_new_days
+		#find all users and for each one:
+		User.all.each do |u|
+		#  create new key value pairs for that day for that user
+			@day = Days.new({
+				budget: u.calculate_daily_budget,
+				transaction_sum: u.yesterday_transactions_sum.to_i
+				})
+			@day.save
+		end
+	end
 end
