18eb7d9948d2b44c36361b2ec5dcd00eee55b85b
ekingan <ekingan@gmail.com>
2015-11-22 14:01:27 -0800

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Day.destroy_all
-# Transaction.destroy_all
+Transaction.destroy_all
 
 # SEED Days 
 # Day.create([
@@ -165,165 +165,165 @@
 # 	}])
 
 # seed transactions
-10.times do 
+ 
 Transaction.create([
 	{ amount: 10,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 20:32:42.486377",
 		category: "Restaurants"
 	},
 	{ amount: 15,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 20:32:42.486377",
 		category: "Groceries"
 	},
 	{ amount: 20,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Transportation"	
 	},
 	{ amount: 25,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Entertainment"	
 	},
 	{ amount: 30,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Coffee"	
 	},
 	{ amount: 35,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Bars"	
 	},
 	{ amount: 40,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Household"	
 	},
 	{ amount: 45,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Clothing"	
 	},
 	{ amount: 50,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Personal"	
 	},
 	{ amount: 55,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Restaurants"	
 	},
 	{ amount: 60,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Groceries"	
 	},
 	{ amount: 65,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Transportation"	
 	},
 	{ amount: 70,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Entertainment"	
 	},
 	{ amount: 75,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Coffee"	
 	},
 	{ amount: 80,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Bars"	
 	},
 	{ amount: 85,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Household"	
 	},
 	{ amount: 90,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Clothing"	
 	},
 	{ amount: 95,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Personal"	
 	},
 	{ amount: 100,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Restaurants"	
 	},
 	{ amount: 105,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Groceries"	
 	},
 	{ amount: 110,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Transportation"	
 	},
 	{ amount: 115,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Entertainment"	
 	},
 	{ amount: 120,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Coffee"	
 	},
 	{ amount: 125,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Bars"	
 	},
 	{ amount: 130,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Household"	
 	},
 	{ amount: 135,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Clothing"	
 	},
 	{ amount: 140,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Personal"	
 	},
 	{ amount: 145,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Restaurants"	
 	},
 	{ amount: 150,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Groceries"	
 	},
 	{ amount: 155,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Transportation"	
 	},
 	{ amount: 160,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Entertainment"	
 	},
 	{ amount: 165,
-		user_id: rand(2..8),
+		user_id: 8,
 		created_at: "2015-11-21 19:00:30.11476",
 		category: "Coffee"		
 	}])
