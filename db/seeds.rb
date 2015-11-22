2a608bfada7cf342ee71bf35cb35005181bbee56
ekingan <ekingan@gmail.com>
2015-11-22 14:06:23 -0800

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,168 +1,168 @@
-# Day.destroy_all
+Day.destroy_all
 Transaction.destroy_all
 
 # SEED Days 
-# Day.create([
-# 	{ budget: 100,
-# 		transactions_sum: 10,
-# 		user_id: 8,
-# 		created_at: "2015-11-21 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 15,
-# 		user_id: 8,
-# 		created_at: "2015-10-23 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 20,
-# 		user_id: 8,
-# 		created_at: "2015-10-24 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 25,
-# 		user_id: 8,
-# 		created_at: "2015-10-25 19:00:30.11476"
-# 	},	
-# 	{ budget: 100,
-# 		transactions_sum: 30,
-# 		user_id: 8,
-# 		created_at: "2015-10-26 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 35,
-# 		user_id: 8,
-# 		created_at: "2015-10-27 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 40,
-# 		user_id: 8,
-# 		created_at: "2015-10-28 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 45,
-# 		user_id: 8,
-# 		created_at: "2015-10-29 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 50,
-# 		user_id: 8,
-# 		created_at: "2015-10-30 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 55,
-# 		user_id: 8,
-# 		created_at: "2015-10-31 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 60,
-# 		user_id: 8,
-# 		created_at: "2015-11-01 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 65,
-# 		user_id: 8,
-# 		created_at: "2015-11-02 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 70,
-# 		user_id: 8,
-# 		created_at: "2015-11-03 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 75,
-# 		user_id: 8,
-# 		created_at: "2015-11-04 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 80,
-# 		user_id: 8,
-# 		created_at: "2015-11-05 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 85,
-# 		user_id: 8,
-# 		created_at: "2015-11-06 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 90,
-# 		user_id: 8,
-# 		created_at: "2015-11-07 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 95,
-# 		user_id: 8,
-# 		created_at: "2015-11-08 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 100,
-# 		user_id: 8,
-# 		created_at: "2015-11-09 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 105,
-# 		user_id: 8,
-# 		created_at: "2015-11-10 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 110,
-# 		user_id: 8,
-# 		created_at: "2015-11-11 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 115,
-# 		user_id: 8,
-# 		created_at: "2015-11-12 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 120,
-# 		user_id: 8,
-# 		created_at: "2015-11-13 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 125,
-# 		user_id: 8,
-# 		created_at: "2015-11-14 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 130,
-# 		user_id: 8,
-# 		created_at: "2015-11-15 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 135,
-# 		user_id: 8,
-# 		created_at: "2015-11-16 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 140,
-# 		user_id: 8,
-# 		created_at: "2015-11-17 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 145,
-# 		user_id: 8,
-# 		created_at: "2015-11-18 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 150,
-# 		user_id: 8,
-# 		created_at: "2015-11-19 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 155,
-# 		user_id: 8,
-# 		created_at: "2015-11-20 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 160,
-# 		user_id: 8,
-# 		created_at: "2015-11-21 19:00:30.11476"
-# 	},
-# 	{ budget: 100,
-# 		transactions_sum: 165,
-# 		user_id: 8,
-# 		created_at: "2015-11-22 19:00:30.11476"
-# 	}])
+Day.create([
+	{ budget: 100,
+		transactions_sum: 10,
+		user_id: 8,
+		created_at: "2015-11-21 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 15,
+		user_id: 8,
+		created_at: "2015-10-23 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 20,
+		user_id: 8,
+		created_at: "2015-10-24 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 25,
+		user_id: 8,
+		created_at: "2015-10-25 19:00:30.11476"
+	},	
+	{ budget: 100,
+		transactions_sum: 30,
+		user_id: 8,
+		created_at: "2015-10-26 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 35,
+		user_id: 8,
+		created_at: "2015-10-27 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 40,
+		user_id: 8,
+		created_at: "2015-10-28 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 45,
+		user_id: 8,
+		created_at: "2015-10-29 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 50,
+		user_id: 8,
+		created_at: "2015-10-30 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 55,
+		user_id: 8,
+		created_at: "2015-10-31 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 60,
+		user_id: 8,
+		created_at: "2015-11-01 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 65,
+		user_id: 8,
+		created_at: "2015-11-02 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 70,
+		user_id: 8,
+		created_at: "2015-11-03 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 75,
+		user_id: 8,
+		created_at: "2015-11-04 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 80,
+		user_id: 8,
+		created_at: "2015-11-05 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 85,
+		user_id: 8,
+		created_at: "2015-11-06 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 90,
+		user_id: 8,
+		created_at: "2015-11-07 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 95,
+		user_id: 8,
+		created_at: "2015-11-08 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 100,
+		user_id: 8,
+		created_at: "2015-11-09 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 105,
+		user_id: 8,
+		created_at: "2015-11-10 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 110,
+		user_id: 8,
+		created_at: "2015-11-11 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 115,
+		user_id: 8,
+		created_at: "2015-11-12 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 120,
+		user_id: 8,
+		created_at: "2015-11-13 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 125,
+		user_id: 8,
+		created_at: "2015-11-14 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 130,
+		user_id: 8,
+		created_at: "2015-11-15 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 135,
+		user_id: 8,
+		created_at: "2015-11-16 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 140,
+		user_id: 8,
+		created_at: "2015-11-17 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 145,
+		user_id: 8,
+		created_at: "2015-11-18 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 150,
+		user_id: 8,
+		created_at: "2015-11-19 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 155,
+		user_id: 8,
+		created_at: "2015-11-20 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 160,
+		user_id: 8,
+		created_at: "2015-11-21 19:00:30.11476"
+	},
+	{ budget: 100,
+		transactions_sum: 165,
+		user_id: 8,
+		created_at: "2015-11-22 19:00:30.11476"
+	}])
 
 # seed transactions
  
